2fc9ba98c8e430e46cc81d891a6b130f2dc85243
bghive <bghive09@gmail.comclear>
2023-03-28 22:51:58 +0530

    initial commit

---
+++ src/main/java/com/bnym/reverseproxy/service/ProxyService.java
@@ -0,0 +1,89 @@
+package com.bnym.reverseproxy.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+import java.util.Map;
+
+@Service
+@ConfigurationProperties(prefix = "proxy-config")
+@Component
+public class ProxyService {
+    private Map<Object, Map<String, String>> urlMapping;
+    private final static Logger logger = LogManager.getLogger(ProxyService.class);
+
+    public Map<Object, Map<String, String>> getUrlMapping() {
+        return urlMapping;
+    }
+
+    public void setUrlMapping(Map<Object, Map<String, String>> urlMapping) {
+        this.urlMapping = urlMapping;
+    }
+
+    public ResponseEntity<String> processProxyRequest(String body,
+                                                      HttpMethod method, HttpServletRequest request, HttpServletResponse response, String traceId) throws URISyntaxException {
+        ThreadContext.put("traceId", traceId);
+        String requestUrl = request.getRequestURI();
+
+        //log if required in this line
+        URI uri = new URI("https", null, urlMapping.get(requestUrl).get("domain"), -1, null, null, null);
+
+        // replacing context path form urI to match actual gateway URI
+        uri = UriComponentsBuilder.fromUri(uri)
+                .path(urlMapping.get(requestUrl).get("path"))
+                .query(request.getQueryString())
+                .build(true).toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headers.set(headerName, request.getHeader(headerName));
+        }
+
+        headers.set("TRACE", traceId);
+        headers.remove(HttpHeaders.ACCEPT_ENCODING);
+
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+        ClientHttpRequestFactory factory = new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+        RestTemplate restTemplate = new RestTemplate(factory);
+        try {
+
+            ResponseEntity<String> serverResponse = restTemplate.exchange(uri, method, httpEntity, String.class);
+            HttpHeaders responseHeaders = new HttpHeaders();
+            responseHeaders.put(HttpHeaders.CONTENT_TYPE, serverResponse.getHeaders().get(HttpHeaders.CONTENT_TYPE));
+            logger.info(serverResponse);
+            return serverResponse;
+
+
+        } catch (HttpStatusCodeException e) {
+            logger.error(e.getMessage());
+            return ResponseEntity.status(e.getRawStatusCode())
+                    .headers(e.getResponseHeaders())
+                    .body(e.getResponseBodyAsString());
+        }
+
+    }
+}
+
