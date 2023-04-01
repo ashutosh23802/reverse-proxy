0c162bdc77707299d3459faf5dddda52b50940c2
bghive <bghive09@gmail.comclear>
2023-04-01 16:06:46 +0530

    read url directly from application.yml

--- src/main/java/com/bnym/reverseproxy/controller/ProxyController.java
+++ src/main/java/com/bnym/reverseproxy/controller/ProxyController.java
@@ -18,9 +18,10 @@ public class ProxyController {
     @Autowired
     ProxyService service;
     @RequestMapping("/**")
-    public ResponseEntity<String> sendRequestToSPM(@RequestBody(required = false) String body,
+    public ResponseEntity<Void> sendRequestToSPM(@RequestBody(required = false) String body,
                                                    HttpMethod method, HttpServletRequest request, HttpServletResponse response)
             throws URISyntaxException {
+
         return service.processProxyRequest(body,method,request,response, UUID.randomUUID().toString());
     }
 }
